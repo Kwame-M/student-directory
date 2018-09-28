e9ecf47e85c1b72ea907e9bee129b3bc364d74d4
Kwame-M <kwame.mintah@cgi.com>
2018-09-28 08:07:20 +0000

    Making student directory.rb to use arrays

--- directory.rb
+++ directory.rb
@@ -1,41 +1,21 @@
 # Array fall of students
-
 students = [
-
     "Dr. Hannibal Lecter",
-
     "Darth Vader",
-
     "Nurse Ratched",
-
     "Michael Corleone",
-
     "Alex DeLarge",
-
     "The Wicked Witch of the West",
-
     "Terminator",
-
     "Freddy Krueger",
-
     "The Joker",
-
     "Joffrey Baratheon",
-
     "Norman Bates"
-
 ]
-
 puts "The students of Villains Academy"
-
 puts "-------------"
-
 students.each do |student|
-
     puts student
-
 end
-
 # total is printed out
-
 puts "Overall, we have #{students.count} great students"
