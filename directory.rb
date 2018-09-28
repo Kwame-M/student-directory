c8edb545a93983c22a5ed313386ab16bebba7a75
Kwame-M <kwame.mintah@cgi.com>
2018-09-28 08:16:08 +0000

    Updating student directory.rb to load data from file

--- directory.rb
+++ directory.rb
@@ -43,3 +43,12 @@ def save_students
   end
   file.close
 end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
